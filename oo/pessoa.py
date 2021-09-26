82c3f87322da35b62083026e8f079f005beb571d
Ricardo Dias Porto <ricardodporto@gmail>
2021-09-26 12:53:57 -0300

    Criado método cumprimentas

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,8 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return 'Olá'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p)) # não é forma usual
+    print(p.cumprimentar()) # passagem implícita. mais usual
